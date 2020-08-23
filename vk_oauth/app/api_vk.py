f54f2d4c8a68c4226d1a883ea0d1c373227c5db1
PavliukKonstantin <Pavliuk.Konstantin@gmail.com>
2020-08-23 16:24:37 +0300

    Rewrite views, add api_vk, add docstrings

---
+++ vk_oauth/app/api_vk.py
@@ -0,0 +1,50 @@
+import vk_api
+
+
+def get_vk_api(access_token: str) -> vk_api.VkApi:
+    """
+    Return API session.
+
+    Args:
+        access_token (str): access token VK application.
+
+    Returns:
+        vk_api.VkApi: session for API methods use.
+    """
+    vk_session = vk_api.VkApi(token=access_token)
+    api = vk_session.get_api()
+    return api
+
+
+def get_account_info(api: vk_api.VkApi) -> dict:
+    """
+    Return authorized account information.
+
+    Args:
+        api (vk_api.VkApi): session for API methods use.
+
+    Returns:
+        dict: authorized account information.
+    """
+    account_info = api.users.get(
+        fields=["photo_200_orig"],
+    )
+    return account_info[0]
+
+
+def get_vk_friends(api: vk_api.VkApi) -> dict:
+    """
+    Return information about 5 random friends.
+
+    Args:
+        api (vk_api.VkApi): session for API methods use.
+
+    Returns:
+        dict: information about 5 random friends.
+    """
+    five_friends = api.friends.get(
+        order="random",
+        count=5,
+        fields=["photo_200_orig"],
+    )
+    return five_friends.get("items")
--- vk_oauth/app/views.py
+++ vk_oauth/app/views.py
@@ -1,54 +1,28 @@
-import vk_api
 from django.shortcuts import render
 
-
-def get_vk_api(access_token):
-    vk_session = vk_api.VkApi(token=access_token)
-    api = vk_session.get_api()
-    return api
+from . import api_vk
 
 
-def get_account_info(api):
-    account_info = api.users.get(
-        fields=["photo_200_orig"],
-    )
-    return account_info
+def vk_friends(request):
+    """
+    Generate html page.
 
-
-def get_vk_friends(api):
-    five_friends = api.friends.get(
-        order='random',
-        count=5,
-        fields=["photo_200_orig"],
-    )
-    return five_friends.get("items")
-
-
-def index(request):
+    Page contains information about authorized user and his 5 random friends.
+    """
     context = {}
     user = request.user
-    # if user.is_authenticated:
-    #     user_methods = user.__dir__
-
-    #     context.update({
-    #         'first_name': user.first_name,
-    #         'last_name': user.last_name,
-    #         'email': user.email,
-    #         "user_methods": user_methods,
-    #     })
 
     if user.is_authenticated:
         user_social_auth = user.social_auth.get(provider='vk-oauth2')
         access_token = user_social_auth.access_token
 
-        api = get_vk_api(access_token)
-        vk_friends = get_vk_friends(api)
-        account_info = get_account_info(api)[0]
-        # user_data = get_user_data(api)
+        api = api_vk.get_vk_api(access_token)
+        vk_friends = api_vk.get_vk_friends(api)
+        account_info = api_vk.get_account_info(api)
 
         context.update({
             "account_info": account_info,
             "vk_friends": vk_friends,
         })
 
-    return render(request, "index.html", context)
+    return render(request, "vk_friends.html", context)
